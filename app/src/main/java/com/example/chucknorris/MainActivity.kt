b76937fe9eb1509bd0ba5b77d686e50f3529ad6b
Tyler Conley <tjacobconley1@gmail.com>
2021-10-18 01:28:02 -0400

    added a coroutine in order to execute the network call on a separate thread in order to reduce UI latency

--- app/src/main/java/com/example/chucknorris/MainActivity.kt
+++ app/src/main/java/com/example/chucknorris/MainActivity.kt
@@ -14,15 +14,15 @@ import io.reactivex.rxjava3.core.Scheduler
 import io.reactivex.rxjava3.schedulers.Schedulers
 import io.reactivex.rxjava3.schedulers.Schedulers.io
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import retrofit2.Response
 import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
-
-    private val API_RESPONSE = ""
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,35 +33,31 @@ class MainActivity : AppCompatActivity() {
 
         setContentView(R.layout.activity_main)
 
-
         // creates an object of the MediaPlayer Class
         // with the whip.mp3 as a sound to play
         val playWhip: MediaPlayer = MediaPlayer.create(this, R.raw.whip)
         playWhip.start()
 
 
-
         // what to execute when the image button is pressed
-        btn_norris.setOnClickListener(View.OnClickListener {
+        btn_norris.setOnClickListener{
 
-            //getResponse()
-            println("debug " + getApi().getQuote())
-            // must pull back next quote from the api and
-            // assign the new quote to the variable
-            // then observe the quote using the text view
-            val text = getApi().getQuote()
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribeOn(Schedulers.io())
-                .subscribe({
-                             //ON NEXT
-                            tv_quote.text = it.body()!!.value
-                            },{
-                            //ON FAILURE
-                            })
+            // launch coroutine
+            // 3 possible coroutine scopes
+            // IO, Main, Default(heavy computational work)
+            CoroutineScope(Dispatchers.IO).launch {
+                DisplayQuote()}
+                // play whip sound for epic user experience
+                playWhip.start()
+            }
 
-            // play whip sound for epic app experience
-            playWhip.start()
-        })
+        // what will be executed when the menu arrow is clicked
+        btn_menu.setOnClickListener{
+
+            //TODO create a second activity
+            //TODO navigate to it lol
+
+        }
 
     }
 
@@ -69,13 +65,48 @@ class MainActivity : AppCompatActivity() {
 
 
 
-    // pulls the quote returned from the Api into
-    // the QUOTE variable and returns it as a string
-    private suspend fun getApiResults(): String{
-
-        val QUOTE = getApi().getQuote()
-        delay(1000)
-        return QUOTE.toString()
+    override fun onStop() {
+        super.onStop()
+        // creates an object of the MediaPlayer Class
+        // with the whip.mp3 as a sound to play
+        val playWhip: MediaPlayer = MediaPlayer.create(this, R.raw.whip)
+        playWhip.start()
+    }
+    override fun onPause() {
+        super.onPause()
+        // creates an object of the MediaPlayer Class
+        // with the whip.mp3 as a sound to play
+        val playWhip: MediaPlayer = MediaPlayer.create(this, R.raw.whip)
+        playWhip.start()
+    }
+    override fun onResume(){
+        super.onResume()
+        // creates an object of the MediaPlayer Class
+        // with the whip.mp3 as a sound to play
+        val playWhip: MediaPlayer = MediaPlayer.create(this, R.raw.whip)
+        playWhip.start()
+    }
+    override fun onDestroy(){
+        super.onDestroy()
+        // creates an object of the MediaPlayer Class
+        // with the whip.mp3 as a sound to play
+        val playWhip: MediaPlayer = MediaPlayer.create(this, R.raw.whip)
+        playWhip.start()
     }
 
+
+    // This function is marked as suspend because it will
+    // be executed within a coroutine on a separate worker
+    // thread in order to reduce UI latency
+    suspend fun DisplayQuote() {
+        val quote = getApi().getQuote()
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribeOn(Schedulers.io())
+            .subscribe({
+                //ON NEXT
+                tv_quote.text = it.body()!!.value
+            }, {
+                //ON FAILURE
+            })
+    }
 }
